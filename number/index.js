a92dd2614149c62398d802247347b9b5defdda28
Kylenn Huang <79785468+kylenn-huang@users.noreply.github.com>
2022-06-13 01:11:33 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+        document.write(i + " is odd number ")
+        document.write("<br/>");
+    } else {
+        document.write(i + " is even number ")
+        document.write("<br/>");
+    }
+}
